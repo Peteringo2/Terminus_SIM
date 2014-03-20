974520b540720b00ff6a98c87bf36bd70ef1e1a2
PedroOM <pedrotrens@gmail.com>
2014-03-20 11:51:49 -0600

    follow por probarse

--- TerminusProyect/src/Grammar.scala
+++ TerminusProyect/src/Grammar.scala
@@ -3,6 +3,8 @@ import scala.io.Source._
 object Grammar {
 	
 	var Grammar:Map[String,List[String]] = Map()
+	val token = ("@[a-zA-Z]+@").r
+	val NonTerminal = ("<[a-zA-Z]+>").r
 	
 	def generateGrammar(GrammarSource:String){
      val GrammarCodeLine = fromFile(GrammarSource).getLines
@@ -37,8 +39,6 @@ object Grammar {
 	
 	def firstTree(i:String):List[String] ={
 	  var Terminal: List[String] = List()
-	  val token = ("@[a-zA-Z]+@").r
-	  val NonTerminal = ("<[a-zA-Z]+>").r
 	  for (x <- Grammar(i)){
               if(x.charAt(0) == '<'){
                 val find = (NonTerminal findFirstIn x).mkString("")
@@ -56,9 +56,41 @@ object Grammar {
 	  
 	}
 	
-	
+
 	def getFollows(GrammarSource:String):Map[String,List[String]]={
-	  var follows:Map[String,List[String]] = Map()
+	  	var follows:Map[String,List[String]] = Map()
+	  	var follows:Map[String, List[String]] = Map()//Mapa que contiene los sets follows
+	  	val changes : Boolean = true //variable para saber si hubo cambios en el ciclo
+	  	Grammar.keys.foreach(key => follows += (key -> List() ) ) //iniciamos follow con todos los no terminales
+	  	follows += ("S" -> List("$")) // inicia el follow de <S> con el símbolo de $
+
+	  	while(changes){
+	  		changes = false
+	  		follows.keys.foreach(prod =>
+	  			Grammar(prod).foreach(x => 
+	  				for(A : String <- NonTerminal findAllIn x){
+	  					index : Int = x(x.indexOfSlice(A) + A.length)
+	  					set = follows(A)
+	  					if(index >= x.length)
+	  						new_set = set ::: follows(x)
+	  					else if(x(index) == "@"){
+	  						//agregamos terminal
+	  					}
+	  					else if(x(index) == "<"){
+	  						val beta = (NonTerminal findFirstIn x.substring(index)).mkString("")
+    		 					new_set = set ::: firsts(beta).filter(a =>  a != "!")
+    		 					if(firsts(beta).contains("!"))
+    		 						new_set = set ::: follows(x)
+	  					}
+	  					if(set != new_set){
+    		 					follows(A) += new_set
+    		 					changes = true
+    		 				}
+	  				}
+	  			)	
+	  		)
+	  	}
+
 	  return follows
     }
 }
