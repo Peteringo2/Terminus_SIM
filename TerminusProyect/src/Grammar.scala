6257f8fe53e9aa0d7020702eb5fc5bccaa7563be
PedroOM <pedrotrens@gmail.com>
2014-03-24 12:56:25 -0600

    follow a probar ya con todo lo de cury

--- TerminusProyect/src/Grammar.scala
+++ TerminusProyect/src/Grammar.scala
@@ -60,40 +60,42 @@ object Grammar {
 	}
 	
 
-//	def getFollows(GrammarSource:String):Map[String,List[String]]={
-//	  	var follows:Map[String,List[String]] = Map()
-//	  	var follows:Map[String, List[String]] = Map()//Mapa que contiene los sets follows
-//	  	val changes : Boolean = true //variable para saber si hubo cambios en el ciclo
-//	  	Grammar.keys.foreach(key => follows += (key -> List() ) ) //iniciamos follow con todos los no terminales
-//	  	follows += ("S" -> List("$")) // inicia el follow de <S> con el símbolo de $
-//
-//	  	while(changes){
-//	  		changes = false
-//	  		follows.keys.foreach(prod =>
-//	  			Grammar(prod).foreach(x => 
-//	  				for(A : String <- NonTerminal findAllIn x){
-//	  					index : Int = x(x.indexOfSlice(A) + A.length)
-//	  					set = follows(A)
-//	  					if(index >= x.length)
-//	  						new_set = set ::: follows(x)
-//	  					else if(x(index) == "@"){
-//	  						//agregamos terminal
-//	  					}
-//	  					else if(x(index) == "<"){
-//	  						val beta = (NonTerminal findFirstIn x.substring(index)).mkString("")
-//    		 					new_set = set ::: firsts(beta).filter(a =>  a != "!")
-//    		 					if(firsts(beta).contains("!"))
-//    		 						new_set = set ::: follows(x)
-//	  					}
-//	  					if(set != new_set){
-//    		 					follows(A) += new_set
-//    		 					changes = true
-//    		 				}
-//	  				}
-//	  			)	
-//	  		)
-//	  	}
-//
-//	  return follows
-//    }
+	def getFollows(GrammarSource:String):Map[String,Set[String]]={
+	  	var follows:Map[String, Set[String]] = Map()//Mapa que contiene los sets follows
+	  	var changes : Boolean = true //variable para saber si hubo cambios en el ciclo
+	  	Grammar.keys.foreach(key => follows += (key -> Set() ) ) //iniciamos follow con todos los no terminales
+	  	follows += ("S" -> Set("$")) // inicia el follow de <S> con el símbolo de $
+
+	  	while(changes){
+	  		changes = false
+	  		follows.keys.foreach(prod =>
+	  			Grammar(prod).foreach(x => 
+	  				for(a : String <- NonTerminal findAllIn x){
+	  					var index : Int = (x.indexOfSlice(a) + a.length)
+	  					var set = follows(a)
+	  					var new_set : Set[String] = Set()
+	  					if(index >= x.length)
+	  						new_set = set.union(follows(x))
+	  					else if(x(index) == '@'){
+	  						//agregamos terminal
+	  					}
+	  					else if(x(index) == '<'){
+	  						val beta = (NonTerminal findFirstIn x.substring(index)).mkString("")
+   		 					new_set = set.union( Firsts(beta).toSet.filter(a =>  a != "!") )
+   		 					if(Firsts(beta).contains("!"))
+   		 						new_set = set.union(follows(x))
+	  					}
+	  					if(set != new_set){
+    		 				var follow_a = follows(a)
+    		 				follow_a = follow_a.union(new_set)
+   		 					follows += (a -> follow_a)
+   		 					changes = true
+   		 				}
+	  				}
+	  			)	
+	  		)
+	  	}
+
+	  return follows
+   }
 }
