9fa6aafe902ca282ec8195353ddc6d749ed21631
PedroOM <pedrotrens@gmail.com>
2014-03-25 00:38:27 -0600

    Generacion de la cerradura con indices completa

--- TerminusProyect/src/BottomUp.scala
+++ TerminusProyect/src/BottomUp.scala
@@ -3,39 +3,47 @@ class BottomUp{
 	
   val tokenator = ("@[a-zA-Z]+@|<[a-zA-Z]+>").r
   
-	def getClosure(charToClosure :String, index : Int): Map[Any, List[(Any,Any)]] = {
+	def getClosure(charToClosure :String): Map[Any, List[(Any,Any)]] = {
 			var map : Map[Any, List[(Any,Any)]] = Map()
 			map += (charToClosure -> List())
 			for(str_chain <- Grammar.Grammar(charToClosure))
 			{
-						var lista_a_procesar = List(tokenator.findFirstIn(str_chain).mkString(""))
-						if(str_chain != "!") lista_a_procesar = getStringsToProcess(str_chain)
-						for(cadena <- lista_a_procesar){
-						cadena charAt index match{
-						case ('@') =>{var temp =  map(charToClosure); temp = temp ::: List((str_chain, 0)); map += (charToClosure -> temp)}
-						case ('<') => { var temp =  map(charToClosure); temp = temp ::: List((str_chain, 0)); map += (charToClosure -> temp)
-						  map ++= getClosure(cadena, 0)
+			  if(str_chain != "!") {
+						//var lista_a_procesar = List(tokenator.findFirstIn(str_chain).mkString(""))
+						var indices = getStringsToProcess(str_chain)
+						for(index <- indices){
+						str_chain charAt index match{
+						case ('@') =>{var temp =  map(charToClosure); temp = temp ::: List((str_chain, index)); map += (charToClosure -> temp)}
+						case ('<') => { var temp =  map(charToClosure); temp = temp ::: List((str_chain, index)); map += (charToClosure -> temp)
+						  map ++= getClosure(Grammar.NonTerminal.findFirstIn(str_chain.substring(index)).mkString(""))
 						}
 						case _ => List((1,1,1))
 						}
 						}
+			 }
 			}
 			map
 	}
   
 	def getStringsToProcess(cadena : String)= {
 	    var cadenas = List(tokenator.findFirstIn(cadena).mkString(""))
+	    var indexes = List(0)
 			var flag = cadenas.last(0) == '<'
-			while(flag)
-				if(Grammar.Grammar(cadenas.last).contains("!")) cadenas :+= tokenator.findFirstIn(cadena.substring(cadenas.last.length)).mkString("")
-			  else flag = false
-			cadenas
+			while(flag){
+				if(Grammar.Grammar(cadenas.last).contains("!")){
+				  indexes :+= indexes.last + cadenas.last.length()
+				  println(indexes)
+				  cadenas :+= tokenator.findFirstIn(cadena.substring(cadenas.last.length)).mkString("")
+				}
+				else flag = false
+			}
+	    indexes
 	}
 
 
 	def nodeClosureCreater(prod : String, name : Int){
 		val n = new Nodo(name)
-		n.addMap(getClosure(prod, 0))
+		n.addMap(getClosure(prod))
 
 		for(x <- n.mapa.keys){
 			println("metroid "+ x + " -> " + n.mapa(x))
