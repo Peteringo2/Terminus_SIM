a8bd276ccdcf11631137baa16e82a14c4db6352d
mario_leal121@hotmail.com <mario_leal121@hotmail.com>
2014-03-27 04:01:32 -0600

    bottomup con pequeños bugs

--- TerminusProyect/src/BottomUp.scala
+++ TerminusProyect/src/BottomUp.scala
@@ -8,7 +8,7 @@ class BottomUp{
   var states = List[Int]()
   var symbols = List[String]()
   var tablaSLR = Map[(Int,String), String]()
-  var cadena = List[String]()
+  var entrada = List[String]()
   var root = new Nodo(0)
   
   def getClosure(charToClosure :String, start : Int): Map[Any, Set[(Any,Any)]] = {	  
@@ -111,57 +111,112 @@ class BottomUp{
 	def getTerminalAndNonTerminalForSLRTable{
 
 		for( n <- lista_nodos; apuntador <-  n.pointers){
-			tablaSLR += (n.name, apuntador._1) -> ("S"+apuntador._2.toString)
+			tablaSLR += (n.name, apuntador._1) -> ("S" + apuntador._2.toString)
 		}
+		tablaSLR += (1, "$") -> ("ACCEPT")
+		tablaSLR += (0, "<S>") -> ("S1")
 	}
 
 	def getReduceForSLRTable{
-		for(n <- lista_nodos; key  <- n.mapa.keys ; prodThis <- n.mapa(key) if (prodThis._1.toString.length.toInt == prodThis._2) ; 
-			follow <- Grammar.Follows(key toString) ; lineIndex <- 0 until listOfRoot.length if listOfRoot(lineIndex) == prodThis){
-		  tablaSLR += (n.name, follow) -> ("R"+ lineIndex.toString)
+	  
+	  //for(x <- listOfRoot) println(x)
+	  
+		for(n <- lista_nodos; key  <- n.mapa.keys ; prodThis <- n.mapa(key) if prodThis._1.toString.length <= prodThis._2.asInstanceOf[Int] ; 
+			follow <- Grammar.Follows(key toString) ; lineIndex <- 0 until listOfRoot.length if listOfRoot(lineIndex) == (key toString, prodThis._1.toString)){
+		  if(follow != "$")
+			  tablaSLR += (n.name, "@"+follow+"@") -> ("R"+ lineIndex.toString)
+			else
+				tablaSLR += (n.name, follow) -> ("R"+ lineIndex.toString)
 		} 
+				tablaSLR += (1, "$") -> ("ACCEPT")
+		tablaSLR += (0, "<S>") -> ("S1")
 	}	
 
 	def SLRParse{
 	  var flag = false
-		states ::= 0
-				cadena ::= "$"
-				while(cadena.head != "$" && !flag){
-					var key = (states.head, cadena.head)
-							if(tablaSLR.exists(_==key)){
+			  for(x <- Lex.tokens) entrada :+= "@" + x(0) + "@"
+			  states ::= 0
+				entrada = this.entrada ::: List("$")
+				//for(z <- entrada) println("zora "+z)
+				while(!flag){
+					var key = (states.head, entrada.head)
+							if(tablaSLR.keys.exists(_==key)){
 								tablaSLR(key).charAt(0) match {
 								case 'S' => {
-									states ::= tablaSLR(key).substring(1).toInt
-											symbols ::= cadena.head
-											cadena = cadena.tail
+											states ::= tablaSLR(key).substring(1).toInt
+											symbols ::= entrada.head
+											entrada = entrada.tail
 								}
 								case 'R' => {
 
-									val sizeOfProd = listOfRoot(tablaSLR(key).substring(1).toInt)._2.length
-
-											for(symbol <- 0 until sizeOfProd){
+//											val sizeOfProd = listOfRoot(tablaSLR(key).substring(1).toInt)._2.length									
+											var produccion = listOfRoot(tablaSLR(key).substring(1).toInt)
+											var num_pop = simbolsToPop(produccion._2, symbols)
+											println("num_pop" + num_pop)
+											for(symbol <- 0 until num_pop){
 												states = states.tail
 														symbols = symbols.tail
 											}
-									symbols ::= listOfRoot(tablaSLR(key).substring(1).toInt)._1
-											states ::= tablaSLR(states.head, symbols.head).toInt
+											
+											symbols ::= listOfRoot(tablaSLR(key).substring(1).toInt)._1
+											println("pit" + (states.head, symbols.head))
+											states ::= tablaSLR((states.head, symbols.head)).substring(1).toInt
+								
 								}
+								
+								case 'A' => {
+								  println("haz sido victorioso")
+								  flag = true
 								}
 							}
-							else 
-								flag = true;
+							}
+							else {
+								println("I AM ERROR")
+								flag = true
+							}
 				}
-	  
-	  if(symbols.head == "S") println("Aceptada")
-	  else println("no aceptada")
+	}
+	
+	def simbolsToPop(c1 : String, c2 : List[String]) : Int = { 
+		var num_to_pop = 0 
+		var num_arr = 0
+		var num_< = 0
+		for(x : Char <- c1.toList){
+		  if(x == '<') num_< += 1
+		  if(x == '@') num_arr += 1
+		}
+		
+		var num_tokens = ((num_arr / 2) + num_<)
+		
+//		println("cadena: " + c1 + "cuantostokens" + num_tokens)
+		
+		while(num_tokens > 0){
+		  var index = 0
+		  var encounters = 0
+		  
+		  var pasada = num_tokens - 1
+		  println("pasada " + pasada + "len " + c2.length)
+		  if(pasada < c2.length){
+		  while(index < c1.length){
+			  var str_com = tokenator.findFirstIn(c1.substring(index)).mkString("")
+			  index += str_com.length
+				if(str_com == c2(pasada)){
+				    num_to_pop += 1
+				    pasada -= 1
+				    if(pasada == -1) return num_to_pop
+				}
+		  }
+		  
+		  }
+		  
+		  num_tokens -= 1
+		}
+		num_to_pop
 	}
 
 	def returnRootToList{
-	  var productions : Set[(_,_)] = Set()
-		for(key <- root.mapa.keys){
-		   productions = root.mapa(key)
-		   for(prod <- productions) listOfRoot :+= (key.toString, prod._1.toString)
-		}
+		for(key <- Grammar.Grammar.keys ; prod <- Grammar.Grammar(key))
+				listOfRoot :+= (key.toString, prod)
 		 
 	}
 }
